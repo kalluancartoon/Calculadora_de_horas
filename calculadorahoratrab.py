63a89be3e767bb327217a6cc0c3c08a2a6690f1e
Kalluan Cartoon <kalluanf@gmail.com>
2023-07-09 01:30:05 -0300

    add da saudação

--- calculadorahoratrab.py
+++ calculadorahoratrab.py
@@ -1,6 +1,9 @@
+nome = input("Por gentileza poderia me informar o seu nome ?")
+saudacao = "Olá" + nome + " " "é um grande previlégio poder te ajudar!"
+print(saudacao)
 horas_dia = input("Quantas horas você trabalha por dia ?")
 horas_semanal = int(horas_dia) * 5
 horas_trabalhadas_por_mes = horas_semanal * 5
 salario_mensal = input("Qual é o seu salario mensal ?")
 valor_hora = int(salario_mensal) / int(horas_trabalhadas_por_mes)
-print(valor_hora)  
+print (valor_hora)   
